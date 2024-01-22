ccac3c3f3c9badfa5bf1132b8001b700d3e13a84
Russell Ahmed-Buehler <rahmedbuehler@wilburellis.com>
2024-01-22 13:09:40 -0600

    example with logging

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,58 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonicModule, Platform } from '@ionic/angular';
+import {
+  Geolocation,
+  PermissionStatus,
+  Position,
+} from '@capacitor/geolocation';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonicModule, CommonModule],
 })
 export class HomePage {
-  constructor() {}
+  permission: PermissionStatus | undefined;
+  geolocationPosition: Position | undefined;
+  navigatorPosition: GeolocationPosition | undefined;
+
+  constructor(private platform: Platform) {}
+
+  requestPermission() {
+    console.log('requestPermission clicked');
+    Geolocation.requestPermissions({
+      permissions: ['location', 'coarseLocation'],
+    })
+      .then((value) => {
+        this.permission = value;
+        console.log('requestPermission success:', value);
+      })
+      .catch((error) => console.log('requestPermission error:', error));
+  }
+
+  geolocation() {
+    console.log('geolocation clicked');
+    Geolocation.getCurrentPosition()
+      .then((value) => {
+        this.geolocationPosition = value;
+        console.log('geolocation success:', value);
+      })
+      .catch((error) => console.log('geolocation error:', error));
+  }
+
+  navigator() {
+    console.log('navigator clicked');
+    this.platform.ready().then(() => {
+      navigator.geolocation.getCurrentPosition(
+        (value) => {
+          this.navigatorPosition = value;
+          console.log('navigator success:', value);
+        },
+        (error) => console.log('navigator error:', error)
+      );
+    });
+  }
 }
